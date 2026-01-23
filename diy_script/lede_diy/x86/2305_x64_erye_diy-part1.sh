42fe2b814bd7f7a99e21b33d63e31a200b8a38ba
gxnas <52808122+gxnas@users.noreply.github.com>
2026-01-23 23:02:33 +0800

    Update 2305_x64_erye_diy-part1.sh

--- diy_script/lede_diy/x86/2305_x64_erye_diy-part1.sh
+++ diy_script/lede_diy/x86/2305_x64_erye_diy-part1.sh
@@ -11,10 +11,11 @@ sed -i "/helloworld/d" feeds.conf.default
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 #sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+#sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
+#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-
+sed -i '$a src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
+sed -i '$a src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
 mkdir wget
 
 cat>rename.sh<<-\EOF
